c99f21deb131576bd1a2622eb3b419776583dcdd
htcardone <htcardone@gmail.com>
2017-11-22 22:56:17 -0200

    Step 3: Create GCE Module.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.htcardone.jokeactivity.JokeActivity;
-import com.htcardone.libjokes.Joker;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,9 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokeActivity.class);
-        intent.putExtra(JokeActivity.EXTRA_JOKE, Joker.getJoke());
-        startActivity(intent);
+        new EndpointAsyncTask().execute(this);
     }
 
 
